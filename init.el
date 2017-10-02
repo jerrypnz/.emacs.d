e42eb108b78f2371aa697d86a761d10836bf857b
Jerry Peng <pr2jerry@gmail.com>
2017-10-02 23:30:07 +1300

    Whitelist monokai theme

--- init.el
+++ init.el
@@ -96,3 +96,17 @@ If argument INTERACTIVE-P is set, log additional information."
 (provide 'init)
 
 ;;; init.el ends here
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   (quote
+    ("7ef2884658a1fed818a11854c232511fa25721c60083a2695e6ea34ce14777ee" default))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
