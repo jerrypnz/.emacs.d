d177a16f56a52177b3acca798131f2af6780fcdc
Jerry Peng <jerry@movio.co>
2017-10-13 17:47:18 +1300

    reformat code

--- config/jp-base/jp-modeline.el
+++ config/jp-base/jp-modeline.el
@@ -4,17 +4,16 @@
 ;; Mode line setup
 (setq-default
  mode-line-format
- '(; Position, including warning for 80 columns
+ '(
+   ;; Position, including warning for 80 columns
    (:propertize "%4l" face mode-line-position-face)
    " : "
    (:eval (propertize "%3c" 'face
                       (if (>= (current-column) 80)
                           'mode-line-80col-face
                         'mode-line-position-face)))
-   ; emacsclient [default -- keep?]
-   mode-line-client
-   "  "
-   ; read-only or modified status
+   "    "
+   ;; read-only or modified status
    (:eval
     (cond (buffer-read-only
            (propertize " RO " 'face 'mode-line-read-only-face))
@@ -22,15 +21,15 @@
            (propertize " ** " 'face 'mode-line-read-write-face))
           (t
            (propertize " -- " 'face 'mode-line-read-write-face))))
-   "    "
-   ; directory and buffer/file name
+   "  "
+   ;; directory and buffer/file name
    (:propertize (:eval (shorten-directory default-directory 24))
                 face mode-line-folder-face)
    (:propertize "%b"
                 face mode-line-filename-face)
-   ; narrow [default -- keep?]
+   ;; narrow [default -- keep?]
    " %n "
-   ; mode indicators: vc, recursive edit, major mode, minor modes, process, global
+   ;; mode indicators: vc, recursive edit, major mode, minor modes, process, global
    (vc-mode vc-mode)
    "  %["
    (:propertize mode-name
@@ -67,44 +66,64 @@
 (make-face 'mode-line-process-face)
 (make-face 'mode-line-80col-face)
 
-(set-face-attribute 'mode-line nil
-    :foreground "gray80" :background "gray20"
-    :inverse-video nil
-    :box '(:line-width 2 :color "gray20" :style nil))
-(set-face-attribute 'mode-line-inactive nil
-    :foreground "gray60" :background "gray20"
-    :inverse-video nil
-    :box '(:line-width 2 :color "gray20" :style nil))
+(set-face-attribute
+ 'mode-line nil
+ :foreground "gray80" :background "gray20"
+ :inverse-video nil
+ :box '(:line-width 2 :color "gray20" :style nil))
 
-(set-face-attribute 'mode-line-read-only-face nil
-    :inherit 'mode-line-face
-    :foreground "#4271ae"
-    :box '(:line-width 2 :color "#4271ae"))
-(set-face-attribute 'mode-line-read-write-face nil
-    :inherit 'mode-line-face
-    :foreground "#c82829"
-    :box '(:line-width 2 :color "#c82829"))
-(set-face-attribute 'mode-line-folder-face nil
-    :inherit 'mode-line-face
-    :foreground "gray60")
-(set-face-attribute 'mode-line-filename-face nil
-    :inherit 'mode-line-face
-    :foreground "#eab700"
-    :weight 'bold)
-(set-face-attribute 'mode-line-position-face nil
-    :inherit 'mode-line-face :height 100)
-(set-face-attribute 'mode-line-mode-face nil
-    :inherit 'mode-line-face
-    :foreground "gray80")
-(set-face-attribute 'mode-line-minor-mode-face nil
-    :inherit 'mode-line-mode-face
-    :foreground "gray40"
-    :height 110)
-(set-face-attribute 'mode-line-process-face nil
-    :inherit 'mode-line-face
-    :foreground "#718c00")
-(set-face-attribute 'mode-line-80col-face nil
-    :inherit 'mode-line-position-face
-    :foreground "black" :background "#eab700")
+(set-face-attribute
+ 'mode-line-inactive nil
+ :foreground "gray60" :background "gray20"
+ :inverse-video nil
+ :box '(:line-width 2 :color "gray20" :style nil))
+
+(set-face-attribute
+ 'mode-line-read-only-face nil
+ :inherit 'mode-line-face
+ :foreground "#4271ae"
+ :box '(:line-width 2 :color "#4271ae"))
+
+(set-face-attribute
+ 'mode-line-read-write-face nil
+ :inherit 'mode-line-face
+ :foreground "#c82829"
+ :box '(:line-width 2 :color "#c82829"))
+
+(set-face-attribute
+ 'mode-line-folder-face nil
+ :inherit 'mode-line-face
+ :foreground "gray60")
+
+(set-face-attribute
+ 'mode-line-filename-face nil
+ :inherit 'mode-line-face
+ :foreground "#eab700"
+ :weight 'bold)
+
+(set-face-attribute
+ 'mode-line-position-face nil
+ :inherit 'mode-line-face :height 100)
+
+(set-face-attribute
+ 'mode-line-mode-face nil
+ :inherit 'mode-line-face
+ :foreground "gray80")
+
+(set-face-attribute
+ 'mode-line-minor-mode-face nil
+ :inherit 'mode-line-mode-face
+ :foreground "gray40"
+ :height 110)
+
+(set-face-attribute
+ 'mode-line-process-face nil
+ :inherit 'mode-line-face
+ :foreground "#718c00")
+
+(set-face-attribute
+ 'mode-line-80col-face nil
+ :inherit 'mode-line-position-face
+ :foreground "black" :background "#eab700")
 
 (provide 'jp-modeline)
