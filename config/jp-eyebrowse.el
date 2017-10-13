98e4e317057c3d6411cdf70f9ba61e0957da9ff6
Jerry Peng <jerry@movio.co>
2017-10-14 10:32:13 +1300

    tweak window layout hydra
    - doesn't show all 9 layouts
    - highlight current layout and shadow other ones

--- config/jp-eyebrowse.el
+++ config/jp-eyebrowse.el
@@ -5,6 +5,7 @@
   :config
   (progn
     (setq eyebrowse-new-workspace 'counsel-projectile-switch-project)
+    (setq eyebrowse-tagged-slot-format "%s [%t]")
     (eyebrowse-mode t)))
 
 (use-package jp-window-layouts
--- config/jp-eyebrowse/jp-window-layouts.el
+++ config/jp-eyebrowse/jp-window-layouts.el
@@ -1,25 +1,39 @@
 (require 'hydra)
 (require 'eyebrowse)
 
+(defun jp-eyebrowse-layout-tag (slot)
+  (let* ((window-configs (eyebrowse--get 'window-configs))
+         (window-config (assoc slot window-configs))
+         (tag (nth 2 window-config))
+         (name (cond
+                ((not tag)          "") ; show nothing if it's not created yet
+                ((= (length tag) 0) "workspace") ; default name if there is no tag
+                (t                  tag)))
+         (face (if (= slot (eyebrowse--get 'current-slot))
+                   '(underline t)
+                 'shadow)))
+    (propertize name 'face face)))
+
 (defhydra jp-window-layouts (:hint nil)
   "
- Layouts^^^^      Actions       ^^^^
- ───────^^^^───── ──────────────^^^^────────────────
- [_1_] [_5_]      [_n_] next    [_p_] previous
- [_2_] [_6_]      [_o_] other   [_x_] close
- [_3_] [_7_]      [_r_] rename
- [_4_] [_8_]      [_q_] quit
-
-current: %s(eyebrowse--get 'current-slot)
+ Actions     ^^    Layouts^^
+ ────────────^^─── ───────^^─────
+ [_n_] next        [_1_] ?1?
+ [_p_] previous    [_2_] ?2?
+ [_o_] other       [_3_] ?3?
+ [_r_] rename      [_4_] ?4?
+ [_x_] close       [_5_] ?5?
+ [_q_] quit        [_6_] ?6?
 "
-  ("1" eyebrowse-switch-to-window-config-1 :exit t)
-  ("2" eyebrowse-switch-to-window-config-2 :exit t)
-  ("3" eyebrowse-switch-to-window-config-3 :exit t)
-  ("4" eyebrowse-switch-to-window-config-4 :exit t)
-  ("5" eyebrowse-switch-to-window-config-5 :exit t)
-  ("6" eyebrowse-switch-to-window-config-6 :exit t)
-  ("7" eyebrowse-switch-to-window-config-7 :exit t)
-  ("8" eyebrowse-switch-to-window-config-8 :exit t)
+  ("1" eyebrowse-switch-to-window-config-1 (jp-eyebrowse-layout-tag 1) :exit t)
+  ("2" eyebrowse-switch-to-window-config-2 (jp-eyebrowse-layout-tag 2) :exit t)
+  ("3" eyebrowse-switch-to-window-config-3 (jp-eyebrowse-layout-tag 3) :exit t)
+  ("4" eyebrowse-switch-to-window-config-4 (jp-eyebrowse-layout-tag 4) :exit t)
+  ("5" eyebrowse-switch-to-window-config-5 (jp-eyebrowse-layout-tag 5) :exit t)
+  ("6" eyebrowse-switch-to-window-config-6 (jp-eyebrowse-layout-tag 6) :exit t)
+  ;; ("7" eyebrowse-switch-to-window-config-7 (jp-eyebrowse-layout-tag 7) :exit t)
+  ;; ("8" eyebrowse-switch-to-window-config-8 (jp-eyebrowse-layout-tag 8) :exit t)
+  ;; ("9" eyebrowse-switch-to-window-config-8 (jp-eyebrowse-layout-tag 9) :exit t)
   ("n" eyebrowse-next-window-config)
   ("p" eyebrowse-prev-window-config)
   ("o" eyebrowse-last-window-config)
