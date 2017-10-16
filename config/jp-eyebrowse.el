9650ca0edaa8f826fbe7e54c086d02b349b29b9e
Jerry Peng <jerry@movio.co>
2017-10-16 23:10:24 +1300

    Use project name as eyebrowse workspace tag

--- config/jp-eyebrowse.el
+++ config/jp-eyebrowse.el
@@ -4,7 +4,17 @@
 (use-package eyebrowse
   :config
   (progn
-    (setq eyebrowse-new-workspace 'counsel-projectile-switch-project)
+    ;; Switch to a project and use its name (shorten) as the tag of
+    ;; the window config TODO `shorten-directory' is defined in
+    ;; `jp-modeline.el' - move it to a common lib
+    (defun jp-eyebrowse-new-project-workspace ()
+      (interactive)
+      (let ((project-name (counsel-projectile-switch-project)))
+        (when (> (length project-name) 0)
+          (eyebrowse-rename-window-config (eyebrowse--get 'current-slot)
+                                          (shorten-directory project-name 32)))))
+
+    (setq eyebrowse-new-workspace 'jp-eyebrowse-new-project-workspace)
     (setq eyebrowse-tagged-slot-format "%s [%t]")
     (eyebrowse-mode t)))
 
