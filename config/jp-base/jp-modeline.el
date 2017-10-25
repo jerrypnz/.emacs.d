c36924a524a72d05f62f23da9ec11cd7b184414a
Jerry Peng <jerry@movio.co>
2017-10-25 21:54:12 +1300

    modeline styling

--- config/jp-base/jp-modeline.el
+++ config/jp-base/jp-modeline.el
@@ -1,36 +1,64 @@
 ;; Taken from http://amitp.blogspot.co.nz/2011/08/emacs-custom-mode-line.html with
 ;; some modifications
 
+
+(defvar jp-modeline-active-window nil)
+
+(defun jp-modeline-set-active-window (windows)
+  (when (not (minibuffer-window-active-p (frame-selected-window)))
+    (setq jp-modeline-active-window (selected-window))))
+
+(add-function :before pre-redisplay-function #'jp-modeline-set-active-window)
+
+(defun jp-modeline-active-p ()
+  (eq jp-modeline-active-window (get-buffer-window)))
+
+(defun jp-modeline-position-and-status ()
+  (let* ((active (jp-modeline-active-p))
+         (status (cond
+                  (buffer-read-only
+                   (propertize " RO " 'face
+                               (if active
+                                   'mode-line-read-only-face
+                                 'mode-line-read-only-inactive-face)))
+                  ((buffer-modified-p)
+                   (propertize " ** " 'face
+                               (if active
+                                   'mode-line-read-write-face
+                                 'mode-line-read-write-inactive-face)))
+                  (t
+                   (propertize " -- " 'face
+                               (if active
+                                   'mode-line-read-write-face
+                                 'mode-line-read-write-inactive-face)))))
+         (lines (propertize (format "%s" (line-number-at-pos))
+                            'face 'mode-line-position-face))
+         (columns (propertize (format "%s" (current-column))
+                              'face
+                              (if (>= (current-column) 80)
+                                  'mode-line-80col-face
+                                'mode-line-position-face))))
+    (format " %s%4s:%-3s" status lines columns)))
+
+(message (jp-modeline-position-and-status))
+
 ;; Mode line setup
 (setq-default
  mode-line-format
  '(
-   ;; Position, including warning for 80 columns
-   (:propertize "%4l" face mode-line-position-face)
-   " : "
-   (:eval (propertize "%3c" 'face
-                      (if (>= (current-column) 80)
-                          'mode-line-80col-face
-                        'mode-line-position-face)))
-   "    "
-   ;; read-only or modified status
-   (:eval
-    (cond (buffer-read-only
-           (propertize " RO " 'face 'mode-line-read-only-face))
-          ((buffer-modified-p)
-           (propertize " ** " 'face 'mode-line-read-write-face))
-          (t
-           (propertize " -- " 'face 'mode-line-read-write-face))))
+   (:eval (jp-modeline-position-and-status))
    "  "
    ;; directory and buffer/file name
-   (:propertize (:eval (shorten-directory default-directory 24))
+   (:propertize (:eval (shorten-directory default-directory 20))
                 face mode-line-folder-face)
    (:propertize "%b"
                 face mode-line-filename-face)
    ;; narrow [default -- keep?]
    " %n "
-   ;; mode indicators: vc, recursive edit, major mode, minor modes, process, global
-   (vc-mode vc-mode)
+   ;; mode indicators: vc, recursive edit, major mode, process, global
+   (:propertize (:eval (replace-regexp-in-string "^ Git[:-]" " " vc-mode))
+                face mode-line-vc-face)
+
    "  %["
    (:propertize mode-name
                 face mode-line-mode-face)
@@ -57,10 +85,13 @@
 
 ;; Extra mode line faces
 (make-face 'mode-line-read-only-face)
+(make-face 'mode-line-read-only-inactive-face)
 (make-face 'mode-line-read-write-face)
+(make-face 'mode-line-read-write-inactive-face)
 (make-face 'mode-line-folder-face)
 (make-face 'mode-line-filename-face)
 (make-face 'mode-line-position-face)
+(make-face 'mode-line-vc-face)
 (make-face 'mode-line-mode-face)
 (make-face 'mode-line-minor-mode-face)
 (make-face 'mode-line-process-face)
@@ -70,24 +101,32 @@
  'mode-line nil
  :foreground "gray80" :background "gray20"
  :inverse-video nil
- :box '(:line-width 1 :color "gray30" :style nil))
+ :box '(:line-width 2 :color "gray20" :style nil))
 
 (set-face-attribute
  'mode-line-inactive nil
- :foreground "gray60" :background "gray20"
+ :foreground "gray20" :background "gray20"
  :inverse-video nil
- :box '(:line-width 1 :color "gray20" :style nil))
+ :box '(:line-width 2 :color "gray20" :style nil))
+
+(set-face-attribute
+ 'mode-line-read-only-inactive-face nil
+ :inherit 'mode-line-face
+ :foreground "#4271ae")
 
 (set-face-attribute
  'mode-line-read-only-face nil
- :inherit 'mode-line-face
- :foreground "#4271ae"
+ :inherit 'mode-line-read-only-inactive-face
  :box '(:line-width 2 :color "#4271ae"))
 
 (set-face-attribute
- 'mode-line-read-write-face nil
+ 'mode-line-read-write-inactive-face nil
  :inherit 'mode-line-face
- :foreground "#c82829"
+ :foreground "#c82829")
+
+(set-face-attribute
+ 'mode-line-read-write-face nil
+ :inherit 'mode-line-read-write-inactive-face
  :box '(:line-width 2 :color "#c82829"))
 
 (set-face-attribute
@@ -107,6 +146,11 @@
 
 (set-face-attribute
  'mode-line-mode-face nil
+ :inherit 'mode-line-face
+ :foreground "gray80")
+
+(set-face-attribute
+ 'mode-line-vc-face nil
  :inherit 'mode-line-face
  :foreground "gray80")
 
