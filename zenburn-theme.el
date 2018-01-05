a4bdbfb39e24f7b50c875d0d5955417601db8ff6
Jerry Peng <jerry@movio.co>
2018-01-05 15:36:24 +1300

    Squashed 'lisp/zenburn/' changes from a26323c..62d91fd
    62d91fd Update git-commit faces
    613f4c6 Set the foreground color for "font-latex-script-char-face" to "zenburn-orange" (#292)
    22068f5 Add more magit-signature-* faces
    69795c5 Update hackernews face names
    
    git-subtree-dir: lisp/zenburn
    git-subtree-split: 62d91fd7c054b0747f1309c012976deb471d4608

--- zenburn-theme.el
+++ zenburn-theme.el
@@ -249,6 +249,7 @@ Also bind `class' to ((class color) (min-colors 89))."
    `(font-latex-italic-face ((t (:foreground ,zenburn-cyan :slant italic))))
    `(font-latex-string-face ((t (:inherit ,font-lock-string-face))))
    `(font-latex-math-face ((t (:foreground ,zenburn-orange))))
+   `(font-latex-script-char-face ((t (:foreground ,zenburn-orange))))
 ;;;;; agda-mode
    `(agda2-highlight-keyword-face ((t (:foreground ,zenburn-yellow :weight bold))))
    `(agda2-highlight-string-face ((t (:foreground ,zenburn-red))))
@@ -553,7 +554,9 @@ Also bind `class' to ((class color) (min-colors 89))."
    '(git-annex-dired-annexed-unavailable ((t (:inherit error :weight normal))))
 ;;;;; git-commit
    `(git-commit-comment-action  ((,class (:foreground ,zenburn-green+1 :weight bold))))
-   `(git-commit-comment-branch  ((,class (:foreground ,zenburn-blue+1  :weight bold))))
+   `(git-commit-comment-branch  ((,class (:foreground ,zenburn-blue+1  :weight bold)))) ; obsolete
+   `(git-commit-comment-branch-local  ((,class (:foreground ,zenburn-blue+1  :weight bold))))
+   `(git-commit-comment-branch-remote ((,class (:foreground ,zenburn-green  :weight bold))))
    `(git-commit-comment-heading ((,class (:foreground ,zenburn-yellow  :weight bold))))
 ;;;;; git-gutter
    `(git-gutter:added ((t (:foreground ,zenburn-green :weight bold :inverse-video t))))
@@ -640,8 +643,8 @@ Also bind `class' to ((class color) (min-colors 89))."
    `(guide-key/key-face ((t (:foreground ,zenburn-green))))
    `(guide-key/prefix-command-face ((t (:foreground ,zenburn-green+1))))
 ;;;;; hackernews
-   '(hackernews-comment-count-face ((t (:inherit link-visited :underline nil))))
-   '(hackernews-link-face          ((t (:inherit link         :underline nil))))
+   '(hackernews-comment-count ((t (:inherit link-visited :underline nil))))
+   '(hackernews-link          ((t (:inherit link         :underline nil))))
 ;;;;; helm
    `(helm-header
      ((t (:foreground ,zenburn-green
@@ -904,6 +907,9 @@ Also bind `class' to ((class color) (min-colors 89))."
    `(magit-signature-good      ((t (:foreground ,zenburn-green))))
    `(magit-signature-bad       ((t (:foreground ,zenburn-red))))
    `(magit-signature-untrusted ((t (:foreground ,zenburn-yellow))))
+   `(magit-signature-expired   ((t (:foreground ,zenburn-orange))))
+   `(magit-signature-revoked   ((t (:foreground ,zenburn-magenta))))
+   '(magit-signature-error     ((t (:inherit    magit-signature-bad))))
    `(magit-cherry-unmatched    ((t (:foreground ,zenburn-cyan))))
    `(magit-cherry-equivalent   ((t (:foreground ,zenburn-magenta))))
    `(magit-reflog-commit       ((t (:foreground ,zenburn-green))))
