b72178a4aacb1ca8e693d8a250fc2cf931bca3ae
Jerry Peng <jerry.peng@smxemail.com>
2018-06-02 21:03:44 +1200

    Change theme to doom-spacegrey

--- config/jp-base.el
+++ config/jp-base.el
@@ -38,7 +38,7 @@
 
     ;; Load the theme (doom-one, doom-molokai, etc); keep in mind that each theme
     ;; may have their own settings.
-    (load-theme 'doom-one t)
+    (load-theme 'doom-spacegrey t)
 
     ;; Enable flashing mode-line on errors
     ;;(doom-themes-visual-bell-config)
