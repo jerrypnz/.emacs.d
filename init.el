6f358c6ebc401d0e673346d9ebc53deb10f7704d
Jerry Peng <jerry.peng@smxemail.com>
2018-07-16 13:18:54 +1200

    Custom variables

--- init.el
+++ init.el
@@ -142,3 +142,30 @@ If argument INTERACTIVE-P is set, log additional information."
 (provide 'init)
 
 ;;; init.el ends here
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(safe-local-variable-values
+   (quote
+    ((eval progn
+           (define-clojure-indent
+             (by :defn)
+             (rate :defn)
+             (where :defn)
+             (tagged :defn)
+             (tagged-all :defn)
+             (changed :defn)
+             (project :defn)
+             (smap :defn)
+             (with :defn)
+             (split :defn)
+             (coalesce :defn)
+             (adjust :defn)))))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
