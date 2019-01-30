626a87c01490f46b2985a76bd9f4f29c1c5005ad
Jerry Peng <jerry.peng@smxemail.com>
2019-01-30 21:49:08 +1300

    Add ivy-posframe again (commented for now)

--- config/jp-ivy.el
+++ config/jp-ivy.el
@@ -40,5 +40,34 @@
   :bind
   ("C-M-s" . jp-counsel-grep-or-swiper-symbol-at-pt))
 
+;; (use-package ivy-posframe
+;;   :straight t
+;;   :config
+;;   (progn
+;;     (push '(t . ivy-posframe-display-at-frame-center) ivy-display-functions-alist)
+;;     (setq ivy-display-function #'ivy-posframe-display-at-frame-center)
+;;     (setq ivy-posframe-hide-minibuffer t)
+;;     (setq ivy-posframe-parameters
+;;           '((no-accept-focus . t)
+;;             (no-focus-on-map . t)
+;;             (internal-border-width . 8)
+;;             (width  . 120)
+;;             (min-height  . 12)
+;;             (vertical-scroll-bars . nil)
+;;             (horizontal-scroll-bars . nil)
+;;             (left-fringe . 10)
+;;             (right-fringe . 10)
+;;             (menu-bar-lines . 0)
+;;             (tool-bar-lines . 0)
+;;             (line-spacing . 0)
+;;             (unsplittable . t)
+;;             (top . 100)
+;;             (mouse-wheel-frame . nil)
+;;             (no-other-frame . t)
+;;             (cursor-type . nil)
+;;             (drag-internal-border . t)
+;;             (no-special-glyphs . t))))
+;;   (ivy-posframe-enable))
+
 (provide 'jp-ivy)
 ;;; jp-ivy.el ends here
