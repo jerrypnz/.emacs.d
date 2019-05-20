53d5892054b58e415364e97e5d3a4d37e617b3d5
Jerry Peng <jerry.peng@smxemail.com>
2019-05-20 16:24:49 +1200

    Fix broken flycheck hydra

--- lisp/base/jp-flycheck-hydra.el
+++ lisp/base/jp-flycheck-hydra.el
@@ -19,8 +19,8 @@
 ;; Taken from https://github.com/abo-abo/hydra/wiki/Flycheck
 (defhydra jp-flycheck
   (:hint nil :foreign-keys warn
-         :pre (progn (setq hydra-lv t) (flycheck-list-errors))
-         :post (progn (setq hydra-lv nil) (quit-windows-on "*Flycheck errors*")))
+         :body-pre (flycheck-list-errors)
+         :post (quit-windows-on "*Flycheck errors*"))
   "Errors"
   ("f"  flycheck-error-list-set-filter                            "filter")
   ("n"  flycheck-next-error                                       "next")
